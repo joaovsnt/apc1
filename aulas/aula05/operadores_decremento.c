cd5a2553c3852ce3abfac007c7447004d506df60
joaovsnt <santos.joaov05@gmail.com>
2024-09-11 12:39:49 +0000

    praticas da aula 05

---
+++ aulas/aula05/operadores_decremento.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+//Entrda
+int numero;
+
+  printf("")
+ //Processamento
+ //mumero = numero + 1;
+  int incremento = numero;
+  int pos_incremento = incremento++;
+  int pre_incremento = ++incremento;
+  
+ //numero = numero - 1;
+  int decremento = numero;
+  int pos_decremento = decremento--;
+  int pre_decremento = --decremento;
+  
+ //Saida
+  printf("O incremento do numero %i é %i\n", numero, incremento);
+  printf("O pre-incremento é %i, e o pos-incremento é %i\n", pre_incremento, pos_incremento);
+  printf("O decremento do numero %i é %i\n", numero, decremento);
+  printf("O pre-decremento é %i, e o pos-decremento é %i\n", pre_decremento, pos_decremento); 
+  return 0; 
+}
